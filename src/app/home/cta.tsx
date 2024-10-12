fcfef4f982bf89cd5922a29761fcf42fa75ac82e
rutdvaj <54937822+rutdvaj@users.noreply.github.com>
2024-10-12 15:26:26 +0530

    now see

--- src/app/home/cta.tsx
+++ src/app/home/cta.tsx
@@ -9,7 +9,7 @@ function Ctacomp() {
   return (
     <div>
       <section className="mq">
-        <div className="flex w-full items-center justify-center py-16 mt-16 [mask-image:linear-gradient(to_top,transparent_10%,#000_30%)]">
+        <div className="flex w-full items-center justify-center py-16 mt-28 [mask-image:linear-gradient(to_top,transparent_10%,#000_30%)]">
           <div className="relative flex w-full max-w-[1000px] flex-col items-center justify-center overflow-hidden rounded-[2rem] border p-10 py-14">
             <div className="z-10 size-24 rounded-[2rem] border bg-white/10 p-6 shadow-2xl backdrop-blur-md dark:bg-black/10 lg:size-32 flex items-center justify-center">
               <div className="w-full h-full flex items-center justify-center">
